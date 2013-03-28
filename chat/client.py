209e380a3358a7dd3393418c5f64264db4689343
Karan <ksk75@cornell.edu>
2013-03-27 20:43:27 -0700

    login done

--- chat/client.py
+++ chat/client.py
@@ -5,7 +5,18 @@ print "...It doesn't do much yet"
 serverInterface = ServerInterface()
 
 #get the person's login information
+login = False
+while login == False:
+    
+    username = raw_input("What is your username? ")
+    password = raw_input("what is your password? ")
+
 #connect to the server
+    login = serverInterface.logIn(username,password)
+    if login == False:
+        print "Invalid username or password. Try again."
+
+
 #have them type a message
 #check the server for messages
 
