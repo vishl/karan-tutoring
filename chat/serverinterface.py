c3d97c65280f772fb8a53b748343a9d8752bc4d3
Vishal Parikh <vparikh1@gmail.com>
2013-03-27 20:13:23 -0700

    server interface stub

--- chat/serverinterface.py
+++ chat/serverinterface.py
@@ -1,3 +1,23 @@
+
+#a dummy server interface
 class ServerInterface:
     def __init__(self):
         pass
+
+    #connection always fails
+    def connect(self, ip):
+        return False
+
+    #log in always fails
+    def logIn(self, username, password):
+        return False
+
+    def getUserList(self):
+        return None
+    
+    def sendMessage(self, message, recipientId):
+        return False
+
+    def checkForMessages(self):
+        return None
+
