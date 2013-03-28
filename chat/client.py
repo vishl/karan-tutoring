98277b5022346c9ea3dab896c90449be0cfc8bae
Vishal Parikh <vparikh1@gmail.com>
2013-03-27 19:36:40 -0700

    added server intface and stubs

--- chat/client.py
+++ chat/client.py
@@ -1,2 +1,11 @@
-print "hello"
+from serverinterface import *
+print "Welcome to KChat!"
+print "...It doesn't do much yet"
+
+serverInterface = ServerInterface()
+
+#get the person's login information
+#connect to the server
+#have them type a message
+#check the server for messages
 
