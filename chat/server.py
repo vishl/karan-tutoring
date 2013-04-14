38f5f2d52486457020cf49d11f1d382b7e005caa
Vishal Parikh <vparikh1@gmail.com>
2013-04-14 16:07:47 -0700

    added features to server

--- chat/server.py
+++ chat/server.py
@@ -3,48 +3,6 @@ import cgi
 import BaseHTTPServer
 from SimpleHTTPServer import SimpleHTTPRequestHandler
 
-class Handler(SimpleHTTPRequestHandler):
-    def do_GET(self):
-        if(self.path=="/"):
-            self.wfile.write("root")
-        elif(self.path=="/login"):
-            self.valid()
-            self.login()
-        else:
-            self.invalid(404, "Invalid path")
-            self.wfile.write("Path = " + self.path)
-    def do_POST(self):
-        ctype, pdict = cgi.parse_header(self.headers.getheader('content-type'))
-        if ctype == 'multipart/form-data':
-            postvars = cgi.parse_multipart(self.rfile, pdict)
-        elif ctype == 'application/x-www-form-urlencoded':
-            length = int(self.headers.getheader('content-length'))
-            postvars = cgi.parse_qs(self.rfile.read(length), keep_blank_values=1)
-        else:
-            postvars = {}
-        if(self.path=="/login"):
-            self.valid()
-            self.login(postvars)
-        else:
-            self.invalid(404, "Invalid path:" + self.path)
-
-    def valid(self):
-        self.send_response(200)
-        self.send_header('Content-type','text/html')
-        self.end_headers()
-    def invalid(self, code, message):
-        self.send_response(code)
-        self.send_header('Content-type','text/html')
-        self.end_headers()
-        self.wfile.write(message)
-
-    def login(self, params):
-        l = params["user"]
-        user = None
-        if(l!=None):
-
-        print params
-
 class DataManager():
     def __init__(self):
         self.users = None
@@ -67,19 +25,89 @@ class DataManager():
         return users
 
 
+dataManager = DataManager()
+
+class Handler(SimpleHTTPRequestHandler):
+    def do_GET(self):
+        if(self.path=="/"):
+            self.wfile.write("root")
+        elif(self.path=="/login"):
+            self.valid()
+            self.login()
+        else:
+            self.invalid(404, "Invalid path")
+            self.wfile.write("Path = " + self.path)
+    def do_POST(self):
+        ctype, pdict = cgi.parse_header(self.headers.getheader('content-type'))
+        if ctype == 'multipart/form-data':
+            postvars = cgi.parse_multipart(self.rfile, pdict)
+        elif ctype == 'application/x-www-form-urlencoded':
+            length = int(self.headers.getheader('content-length'))
+            postvars = cgi.parse_qs(self.rfile.read(length), keep_blank_values=1)
+        else:
+            postvars = {}
+        if(self.path=="/login"):
+            self.valid()
+            self.login(postvars)
+        if(self.path=="/sendmessage"):
+            self.valid()
+            self.send(postvars)
+        else:
+            self.invalid(404, "Invalid path:" + self.path)
+
+    def valid(self):
+        self.send_response(200)
+        self.send_header('Content-type','text/html')
+        self.end_headers()
+    def invalid(self, code, message):
+        self.send_response(code)
+        self.send_header('Content-type','text/html')
+        self.end_headers()
+        self.wfile.write(message)
+
+    def login(self, params):
+        l = params["user"]
+        user = None
+        password = None
+        if(l!=None):
+            user = l[0]
+        l = params["password"]
+        if(l!=None):
+            password = l[0]
+        print "Attempting login: User = %s, Password = %s" % (user, password)
+        if user in dataManager.users and dataManager.users[user] == password and password != None:
+            print "success"
+            self.wfile.write("success")
+        else:
+            print "failure"
+            self.wfile.write("failure")
+    def send(self, params):
+        success = True
+        if("senderId" in params and "recipientId" in params and "message" in params):
+            if(params["recipientId"][0] in dataManager.users):
+                print "Sending from: %s to: %s message %s" % (params["senderId"][0], params["recipientId"][0], params["message"][0])
+            else:
+                success = False
+        else:
+            success = False
+        if(success):
+            self.wfile.write("success")
+        else:
+            self.wfile.write("failure")
+            
+
 
 #Setup
 #HandlerClass = SimpleHTTPRequestHandler
 HandlerClass = Handler
 ServerClass  = BaseHTTPServer.HTTPServer
 Protocol     = "HTTP/1.0"
-dataManager = DataManager()
 
 if sys.argv[1:]:
     port = int(sys.argv[1])
 else:
     port = 8000
-server_address = ('127.0.0.1', port)
+server_address = ('', port)
 
 #load data
 users = dataManager.getUsers()
--- chat/servertest.py
+++ chat/servertest.py
@@ -2,10 +2,14 @@ import urllib
 
 params = {}
 
-params['user'] = 'user1'
-params['password'] = 'password'
+#params['user'] = 'user1'
+#params['password'] = 'password'
+
+params['senderId'] = 'user1'
+params['recipienId'] = 'user2'
+params['message'] = 'hello'
 
 params = urllib.urlencode(params)
 print params
-f = urllib.urlopen("http://localhost:8000/login", params)
+f = urllib.urlopen("http://localhost:8000/sendmessage", params)
 print f.read()
