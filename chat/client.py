59ba266612fd719eb584aa4ec527469ce7c0dbae
Karan <ksk75@cornell.edu>
2013-04-14 17:21:02 -0700

    check done

--- chat/client.py
+++ chat/client.py
@@ -1,9 +1,14 @@
-from serverinterfacesim import *
+from serverinterfacehttp import *
+import sys
 print "Welcome to KChat!"
 print "...It doesn't do much yet"
-
+if len(sys.argv) == 2:
+    host = sys.argv[1]
+else:
+    host ="localhost"
+print "host is", host
 serverInterface = ServerInterface()
-
+serverInterface.connect(host+":8000")
 #get the person's login information
 login = False
 while login == False:
@@ -18,6 +23,36 @@ while login == False:
 
 serverInterface.setStatus('online')
 print "You are logged in as",username
-#have them type a message
+
+cfm = serverInterface.checkForMessages(username)
+n=len(cfm)
+print "You have %i unread messages"%(n)
+while True:
+    m = raw_input("Would you like to see the messages or create amessage? Type c to check messages or n to create a message. Type q to quit. ")
+
+    if m=='c':
+    
+        cfm+=serverInterface.checkForMessages(username)
+        for message in cfm:
+
+            print message["from"]
+            print message["message"]
+            
+            reply = raw_input("Would you like to reply to this message? Type yes or no ")
+            if reply == 'yes':
+                replymessage = raw_input('Type your message here: ')
+                sendmessage = serverInterface.sendMessage(replymessage,message["from"],username)
+                print "Your message has been sent."
+        cfm = []
+    if m=='n':
+        sendid = raw_input("Type the username of the person you would like to send a message. ")
+        sendm = raw_input("Type your message. ")
+        sendmessage = serverInterface.sendMessage(sendm,sendid,username)
+        print "Your message has been sent."
+   
+    if m == 'q':
+       print "Goodbye Come back soon." 
+       break
+
 #check the server for messages
 
---
+++ chat/serverinterfacehttp.py
@@ -0,0 +1,72 @@
+import urllib
+import json
+
+class ServerInterface:
+    def __init__(self):
+        self.read = False
+
+    #connection always succeeds
+    def connect(self, ip):
+        self.ip = ip
+        return True
+
+    #log in always 
+        if f.read()=='success':
+            return True
+        else:
+            return False
+    def logIn(self, username, password):
+        
+        params = {}
+
+        params['user'] = username
+        params['password'] =password
+
+        params = urllib.urlencode(params)
+        
+        f = urllib.urlopen("http://"+self.ip+"/login", params)
+        status = f.read()
+        print status
+        if status == 'success':     
+            return True
+        else:
+            
+            return False
+    
+    def getUserList(self):
+        return ["friend1", "friend2"]
+    
+    def sendMessage(self, message, recipientId, senderId):
+        params = {}
+
+        params['message']= message
+        params['recipientId']=recipientId
+        params['senderId']=senderId
+
+        params = urllib.urlencode(params)
+
+        f=urllib.urlopen("http://"+self.ip+"/sendmessage", params)
+
+        if f.read()=='success':
+            return True
+        else:
+            return False
+
+    def checkForMessages(self,recipientId):
+      
+        params = {}
+
+        params['recipientId']=recipientId
+
+        params = urllib.urlencode(params)
+
+
+        f=urllib.urlopen("http://"+self.ip+"/checkmessage", params)
+      
+        data = f.read()
+        return json.loads(data)
+
+
+    def setStatus(self, status):
+        return True
+
